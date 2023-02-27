bd7758ad7788878fdf9914b39729783df87ae766
pradyunramesh <pradyun.ramesh@gmail.com>
2023-02-26 21:31:59 -0500

    Corrections_1

--- cli.js
+++ cli.js
@@ -2,8 +2,9 @@
 import moment from "moment-timezone";
 import minimist from "minimist";
 import fetch from "node-fetch";
+import process from 'process';
 const timezone = moment.tz.guess()
-var input = mini(process.argv.slice(2))
+var input = minimist(process.argv.slice(2))
 if (input.h){
     let message = 'Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE' +
     '-h            Show this help message and exit.' + 
